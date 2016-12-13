596aa826f6c626dd5621279c58afd4964c89d3df
Marek <mkarwowski@equalexperts.com>
2016-12-13 14:12:50 +0000

    DD prefix for button links

--- src/pages/dataset/Details.jsx
+++ src/pages/dataset/Details.jsx
@@ -22,10 +22,10 @@ class Details extends Component {
                     <h1 className="margin-bottom-md--0">{this.props.title}</h1>
                     <p className="page-intro__content margin-bottom-md--1">{this.props.data}</p>
 
-                    <Link to={`/dataset/${this.props.params.id}/download`}
+                    <Link to={`/dd/dataset/${this.props.params.id}/download`}
                           className="btn btn--primary btn--thick btn--big btn--wide">Download the complete dataset &gt;</Link>
                     <br />
-                    <Link to={`/dataset/${this.props.params.id}/customise`}
+                    <Link to={`/dd/dataset/${this.props.params.id}/customise`}
                           className="btn btn--primary btn--thick btn--big btn--wide margin-top--2">Customise this dataset</Link>
 
                     <p className="margin-bottom--0 margin-top--4">
