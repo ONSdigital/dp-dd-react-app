94952110ae675a60f9005c1bea30f2cacdc7bcdc
Marek <mkarwowski@equalexperts.com>
2016-12-19 08:54:36 +0000

    Download page placeholder

--- src/pages/dataset/Download.jsx
+++ src/pages/dataset/Download.jsx
@@ -1,3 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (props) => <h2>Dpwnload</h2>;
+export default class Download extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1 className="margin-top--2 margin-bottom--half">Download options</h1>
+                <p className="page-intro__content margin-top--half margin-bottom--4">Choose the file types you want to download.</p>
+                <p><i>Not implemented yet.</i></p>
+            </div>
+        )
+    }
+}
