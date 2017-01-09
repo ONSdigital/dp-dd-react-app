6729bc5df2818a168d2e721dc5e6cf9f5d0f6c40
Marek <mkarwowski@equalexperts.com>
2017-01-09 10:58:46 +0000

    Allowing global configuration

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,12 @@
-
 if (!window.location.origin) {
     window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
 
-export default Object.assign({
+const defaults = {
     API_URL: 'http://localhost:20099',
     JOB_API_URL: 'http://localhost:20100',
     BASE_URL: window.location.origin,
     BASE_PATH: '/dd'
-}, typeof __CONFIG__ !== 'undefined' ? __CONFIG__ : {});
+}
+const globals = typeof __CONFIG__ !== 'undefined' ? __CONFIG__ : {}
+export default Object.assign(defaults, globals);
