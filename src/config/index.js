22cdb5626d2442d167609dfa9cf6d70d606cdf95
Crispioso <crispin.merriman@gmail.com>
2017-01-10 13:19:57 +0000

    Add environment variables from window to config

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,16 @@
+import Global from 'react-global';
+
 if (!window.location.origin) {
     window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
 
+const appConfig = Global.get('appConfig');
 const defaults = {
-    API_URL: 'http://localhost:20099',
-    JOB_API_URL: 'http://localhost:20100',
+    API_URL: appConfig.API_URL,
+    JOB_API_URL: appConfig.JOB_API_URL,
     BASE_URL: window.location.origin,
-    BASE_PATH: '/dd'
-}
-const globals = typeof __CONFIG__ !== 'undefined' ? __CONFIG__ : {}
+    BASE_PATH: appConfig.BASE_PATH
+};
+
+const globals = typeof __CONFIG__ !== 'undefined' ? __CONFIG__ : {};
 export default Object.assign(defaults, globals);
