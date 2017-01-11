6c2ca8d4d537933306dc29d89772ab039a66cb0a
Marek <mkarwowski@equalexperts.com>
2017-01-11 17:04:30 +0000

    Configuration update

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,13 @@
 if (!window.location.origin) {
-    window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+    const port = (window.location.port ? ':' + window.location.port: '');
+    window.location.origin = window.location.protocol + "//" + window.location.hostname + port;
 }
 
-
 const defaults = {
-    API_URL: appConfig.API_URL,
-    JOB_API_URL: appConfig.JOB_API_URL,
+    API_URL: 'http://localhost:20099',
+    JOB_API_URL: 'http://localhost:20100',
     BASE_URL: window.location.origin,
-    BASE_PATH: appConfig.BASE_PATH
+    BASE_PATH: '/dd'
 };
 
-const globals = typeof __CONFIG__ !== 'undefined' ? __CONFIG__ : {};
-export default Object.assign(defaults, globals);
+export default Object.assign(defaults, appConfig || {});
