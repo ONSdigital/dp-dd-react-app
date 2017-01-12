7a07cf46137afb1c2d47dc7b608f6c706225e8a4
Jon Dewi Jones <jondewijones@gmail.com>
2017-01-12 12:36:52 +0000

    Add suporting files component

---
+++ src/components/elements/SupportingFilesList.jsx
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react';
+
+
+const propTypes = {
+    supportingFiles: PropTypes.arrayOf(React.PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        uri: PropTypes.string.isRequired,
+    }))
+}
+
+export default class SupportingFilesList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderSupportingFiles() {
+        const supportingFiles = this.props.supportingFiles || [];
+        if (supportingFiles.length > 0) {
+            return supportingFiles.map((item, key) => {
+                return (
+                    <li key={key}><a href={item.uri} target="_blank">{item.title}</a></li>
+                )
+                }
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="">Supporting information</h2>
+                <ul>
+                    {/* TODO: Background notes will link to React page that generates content from API - once API is ready */}
+                    <li><a href="./files/background-notes.pdf" target="_blank">Background notes</a></li>
+                    { this.renderSupportingFiles() }
+                </ul>
+            </div>
+        )
+    }
+}
+
+SupportingFilesList.propTypes = propTypes;
