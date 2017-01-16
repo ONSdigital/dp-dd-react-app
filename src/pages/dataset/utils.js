0ccf5a966c5fc3b3049dbfc81e3e7a15f8717706
Marek <mkarwowski@equalexperts.com>
2017-01-16 14:43:04 +0000

    Content-Type header added to POST request

--- src/pages/dataset/utils.js
+++ src/pages/dataset/utils.js
@@ -29,11 +29,20 @@ export class Request {
     }
 
     post (url, opts) {
+        const headers = opts.headers ? opts.headers : {};
+        opts.headers = Object.assign({}, headers, {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        });
         return this.fetch(url, Object.assign({}, opts, { method: 'POST' }));
     }
 
     fetch(url, opts = {}) {
-        return fetch(url, Object.assign({}, this.opts, opts))
+
+        // todo: IMPORTANT!!!! use deepmerge instead of object assign
+
+        const fetchOpts = Object.assign({}, this.opts, opts);
+        return fetch(url, fetchOpts)
             .then(this.checkResponseStatus)
             .then(this.parseResponseToJSON)
             .catch(function (err) {
