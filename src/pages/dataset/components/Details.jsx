b9eda064153579d6089aeb2331f4b580a6f84bd7
Marek <mkarwowski@equalexperts.com>
2017-01-20 14:08:05 +0000

    Additional information added to detail page

--- src/pages/dataset/components/Details.jsx
+++ src/pages/dataset/components/Details.jsx
@@ -39,26 +39,55 @@ class Details extends Component {
         }
 
         return (
-            <div className="wrapper">
-                <div className="col--lg-two-thirds">
-                    <DocumentTitle title={page.title}>
-                        <h1 className="margin-bottom-md--0">{page.title}</h1>
-                    </DocumentTitle>
+            <div>
+                <div className="meta-wrap">
+                    <div className="wrapper">
+                        <div className="col-wrap">
+                            <p className="col col--md-16 col--lg-20 meta__item">
 
-                    <p className="page-intro__content margin-bottom-md--1">{page.description}</p>
+                                <a className="icon--hide"
+                                   href="https://www.statisticsauthority.gov.uk/national-statistician/types-of-official-statistics/"
+                                   target="_blank">
+                                    <img className="meta__image"
+                                         src="https://www.ons.gov.uk/img/national-statistics.png"
+                                         alt="National Statistics logo" />
+                                </a>
 
-                    <Link to={this.state.downloadPath}
-                          className="btn btn--primary btn--thick btn--big btn--wide margin-top--2 font-size--17">
-                            <strong>Download the complete dataset</strong>
-                    </Link>
-                    <br />
-                    <Link to={this.state.customisePath}
-                          className="btn btn--primary btn--thick btn--big btn--wide margin-top--2 font-size--17">
-                            <strong>Customise this dataset</strong>
-                    </Link>
+                                <span>Contact: </span><br />
+                                <a href="mailto:census.customerservices@ons.gsi.gov.uk" data-ga-event="" data-ga-event-category="mailto" data-ga-event-label="census.customerservices@ons.gsi.gov.uk">Alexa Bradley</a>
+                            </p>
+                            <p className="col col--md-16 col--lg-20 meta__item">
+                                <span>Release date: </span><br />
+                                23 May 2014<br />
+                            </p>
+                            <p className="col col--md-16 col--lg-20 meta__item">
+                                <span>Next release: </span>
+                                <br />To be announced
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="wrapper">
+                    <div className="col--lg-two-thirds">
+                        <DocumentTitle title={page.title}>
+                            <h1 className="margin-bottom-md--0">{page.title}</h1>
+                        </DocumentTitle>
 
-                    <div className="margin-bottom--8">
-                        <SupportingFilesList/>
+                        <p className="page-intro__content margin-bottom-md--1">{page.description}</p>
+
+                        <Link to={this.state.downloadPath}
+                              className="btn btn--primary btn--thick btn--big btn--wide margin-top--2 font-size--17">
+                                <strong>Download the complete dataset</strong>
+                        </Link>
+                        <br />
+                        <Link to={this.state.customisePath}
+                              className="btn btn--primary btn--thick btn--big btn--wide margin-top--2 font-size--17">
+                                <strong>Customise this dataset</strong>
+                        </Link>
+
+                        <div className="margin-bottom--8">
+                            <SupportingFilesList/>
+                        </div>
                     </div>
                 </div>
             </div>
