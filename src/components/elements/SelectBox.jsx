9de2d1d9caa23dced344a9a7f73fb8f9a74ff8fb
Jon Dewi Jones <jondewijones@gmail.com>
2017-01-25 13:47:34 +0000

    Add select box react component

---
+++ src/components/elements/SelectBox.jsx
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class SelectBox extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const selectOptions = this.props.options;
+        return (
+            <div className={"select-box" + (this.props.inline ? " select-box--inline" : "")}>
+                <label className={"select-box" + (this.props.hideLabel ? " visuallyhidden " : "")} htmlFor={this.props.id}>
+                    {this.props.label}
+                </label>
+                <select id={this.props.id} className="select-box__input">
+                    {
+                        selectOptions.map((option, key) => {
+                            return <option key={key} value={option.id}>{option.value}</option>
+                        })
+
+                    }
+                </select>
+            </div>
+        )
+    }
+
+}
+
+// SelectBox.propTypes = propTypes;
