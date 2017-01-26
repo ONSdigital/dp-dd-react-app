ba144ae1b10c87449098c19c53e11f9b5873291b
Marek <mkarwowski@equalexperts.com>
2017-01-26 16:00:20 +0000

    Dimensions component externalised

--- src/pages/dataset/routes.js
+++ src/pages/dataset/routes.js
@@ -3,10 +3,11 @@ import config from '../../config';
 import Details from './components/Details';
 import Download from './components/Download';
 import Dimension from './components/Dimension';
+import Dimensions from '../dimensions/components/Dimensions';
 
 export default [
     { path: `${config.BASE_PATH}/datasets/:id`, component: Details },
     { path: `${config.BASE_PATH}/datasets/:id/download`, component: Download },
-    { path: `${config.BASE_PATH}/datasets/:id/dimensions`, component: Dimension },
+    { path: `${config.BASE_PATH}/datasets/:id/dimensions`, component: Dimensions },
     { path: `${config.BASE_PATH}/datasets/:id/dimensions/:dimensionID`, component: Dimension }
 ]
--- src/pages/dimensions/components/Dimensions.jsx
+++ src/pages/dimensions/components/Dimensions.jsx
@@ -2,19 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import config from '../../../config';
-
-import Browser from '../../dimension/components/Browser';
-import Customisation from '../../dimension/components/Customisation';
-import Search from '../../dimension/components/Search';
-import Summary from '../../dimension/components/Summary';
-
 import {
     requestMetadata,
     requestDimensions
-} from '../actions';
+} from '../../dataset/actions';
 
-
-import Selector from '../../dimension/components/DimensionSelector';
+import DimensionList from './DimensionList';
+import DocumentTitle from '../../../components/elements/DocumentTitle';
 
 const propTypes = {
     hasDimensions: PropTypes.bool.isRequired,
@@ -55,47 +49,28 @@ class Dimension extends Component {
 
     render() {
         const props = this.props;
-        if (!props.hasDimensions) return null;
-        const defaultProps = {
-            datasetID: props.params.id,
-            dimensionID: props.params.dimensionID,
-            location: props.location
-        }
-        const componentProps = Object.assign({}, props, defaultProps);
-
-        switch (props.location.query.action) {
-            case 'customise':
-                return <Customisation {...componentProps} />;
-            case 'browse':
-                return <Browser {...componentProps} />;
-            case 'search':
-                return <Search {...componentProps} />;
-            case 'summary':
-                return <Summary {...componentProps} />;
-        }
-        return this.renderDimensionSelector();
-    }
-
-    renderDimensionSelector() {
-        if (!this.props.hasDimensions) {
+        if (!this.props.hasMetadata || !props.hasDimensions) {
             return null;
         }
-
-        const parentPath = this.state.currentPath;
-        const selectorProps = {
-            router: this.props.router,
-            datasetID: this.props.params.id,
-            dimensionID: this.props.params.dimensionID,
-            onSave: () => this.props.router.push(this.state.currentPath)
-        }
+        const parentPath = this.state.parentPath;
+        const dimensions = this.props.dimensions;
 
         return (
-            <div>
+            <div className="wrapper">
                 <div className="margin-top--2">
                     <Link to={parentPath} className="btn--everything">Back</Link>
+                    <DocumentTitle title={`Customise ${this.props.title}`}>
+                        <h2 className="margin-top--half margin-bottom">Customise this dataset</h2>
+                    </DocumentTitle>
                 </div>
                 <div>
-                    <Selector {...selectorProps} />
+                    <DimensionList dimensions={dimensions} />
+                    <div className="margin-top--4 margin-bottom--8">
+                        <Link className="btn btn--primary btn--thick btn--wide btn--big margin-right--half"
+                              to={this.state.downloadPath}>Choose a download format</Link>
+                        <Link className="btn btn--secondary btn--thick btn--wide btn--big"
+                              to={this.state.parentPath}>Cancel</Link>
+                    </div>
                 </div>
             </div>
         )
