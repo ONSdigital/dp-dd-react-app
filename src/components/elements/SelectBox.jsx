1ab4dc771e60b9409759a63dd1c5699bb5c02a9d
Marek <mkarwowski@equalexperts.com>
2017-02-02 14:33:15 +0000

    Cleanup

--- src/components/elements/SelectBox.jsx
+++ src/components/elements/SelectBox.jsx
@@ -33,24 +33,19 @@ export default class SelectBox extends Component {
 
     render() {
         const selectOptions = this.props.options;
+        const selectClass = "select-box__input" + (this.props.inline ? " margin-right--1" : "");
+        const labelClass = "select-box" + (this.props.hideLabel ? " visuallyhidden " : "");
         return (
             <div className={"select-box" + (this.props.inline ? " select-box--inline" : "")}>
-                <label className={"select-box" + (this.props.hideLabel ? " visuallyhidden " : "")} htmlFor={this.props.id}>
+                <label className={labelClass} htmlFor={this.props.id}>
                     {this.props.label}
                 </label>
-                <select id={this.props.id} onChange={this.onChange}
-                        className={"select-box__input" + (this.props.inline ? " margin-right--1" : "")}>
-                    {
-                        selectOptions.map((option, key) => {
-                            return <option key={key} value={option.id}>{option.value}</option>
-                        })
-
-                    }
+                <select id={this.props.id} onChange={this.onChange} className={selectClass}>
+                    {selectOptions.map((option, key) => <option key={key} value={option.id}>{option.value}</option>)}
                 </select>
             </div>
         )
     }
-
 }
 
 SelectBox.propTypes = propTypes;
