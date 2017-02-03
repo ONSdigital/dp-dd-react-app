5b462645f1bf60b0d9b8da5c581a648976fe0142
Marek <mkarwowski@equalexperts.com>
2017-02-03 14:12:11 +0000

    Caching values of individual values

--- src/pages/dimension/components/TimeSelector.jsx
+++ src/pages/dimension/components/TimeSelector.jsx
@@ -10,57 +10,60 @@ class TimeSelector extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
             selectedInterval: 'month',
+            selectedValues: [] // should either contain 1 item for single date of 2 for range
         };
-
-        this.handleChange = this.handleChange.bind(this);
+        this.onIntervalRadioChange = this.onIntervalRadioChange.bind(this);
+        this.onRangeChange = this.onRangeChange.bind(this);
+        this.onAddButtonClick = this.onAddButtonClick.bind(this);
     }
 
-    handleChange(event) {
+    onIntervalRadioChange(event) {
         this.setState({selectedInterval: event.value});
     }
 
+    onRangeChange(selectedValueIndex) {
+        return optionData => {
+            const selectedValues = this.state.selectedValues;
+            selectedValues[selectedValueIndex] = optionData;
+            this.setState({ selectedValues });
+        }
+    }
+
+    onAddButtonClick() {
+        console.log('save it');
+    }
 
     render() {
         if (!this.props.isReady) {
             return null;
         }
-
-        const intervalSelector = [{
-                id: 'month',
-                value: 'month',
-                label: 'Single month'
-            },{
-            id: 'range',
-            value: 'range',
-            label: 'Range'
-        }];
         const selectedInterval = this.state.selectedInterval;
+        const intervalSelector = [
+            { id: 'month', value: 'month', label: 'Single month' },
+            { id: 'range', value: 'range', label: 'Range' }
+        ];
+
         return (
             <form className="form">
                 <h2 className="margin-top--half margin-bottom">Add a single month or range</h2>
                 <div className="margin-top-md--1 margin-bottom-md--2">
                 {intervalSelector.map((radio, index) => {
-                    return <Radio key={index}  {...radio} group={'interval-selector'} onChange={this.handleChange}
-                                      checked={selectedInterval === radio.value} inline={true}/>
+                    return <Radio key={index}  {...radio} onChange={this.onIntervalRadioChange}
+                                  group={'interval-selector'} checked={selectedInterval === radio.value} inline={true}/>
                 })}
                 </div>
 
                 {this.renderTimeSelector()}
 
                 <div className="margin-top--4 margin-bottom--8">
-                    <a className="btn btn--primary btn--thick btn--wide btn--big margin-right--half"
-                       onClick="">Add</a>
+                    <a className="btn btn--primary btn--thick btn--wide btn--big margin-right--half" onClick={this.onAddButtonClick}>Add</a>
                     <a className="btn btn--secondary btn--thick btn--wide btn--big" onClick={hashHistory.goBack}>Cancel</a>
                 </div>
-
-
             </form>
         )
     }
-
 
     renderTimeSelector() {
         const selectedInterval = this.state.selectedInterval;
@@ -73,18 +76,17 @@ class TimeSelector extends Component {
     }
 
     renderMonthSelector() {
-        return <TimeRangeSelector options={this.props.options} />
+        return <TimeRangeSelector options={this.props.options} onChange={this.onRangeChange(0)} />
     }
 
     renderRangeSelector() {
         return (
             <div>
-                <TimeRangeSelector options={this.props.options} />
-                <TimeRangeSelector options={this.props.options} />
+                <TimeRangeSelector options={this.props.options} onChange={this.onRangeChange(0)} />
+                <TimeRangeSelector options={this.props.options} onChange={this.onRangeChange(1)} />
             </div>
         )
     }
-
 }
 
 function mapStateToProps(state, ownProps) {
