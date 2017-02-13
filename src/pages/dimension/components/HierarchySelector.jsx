9ec2d32685201b6a77d038e31f973956e1fff796
Marek <mkarwowski@equalexperts.com>
2017-02-13 11:04:05 +0000

    Hierarchy selector rendered with buttons

--- src/pages/dimension/components/HierarchySelector.jsx
+++ src/pages/dimension/components/HierarchySelector.jsx
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
+import { hashHistory } from 'react-router';
 import Checkbox from '../../../components/elements/Checkbox';
+import ToggleLink from '../../../components/elements/ToggleLink';
 
 export default class HierarchySelector extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            cachedOptions: []
+            cachedOptions: [],
+            allEnabled: false,
+            allDisabled: false
         }
 
         this.cacheSelectedOption = this.cacheSelectedOption.bind(this);
@@ -25,6 +29,24 @@ export default class HierarchySelector extends Component {
         }
 
         this.setState({ cachedOptions });
+    }
+
+    getEnabledStatus() {
+        let allEnabled = false;
+        let allDisabled = allDisabled = this.state.cachedOptions.length === 0;
+
+        return {allEnabled, allDisabled}
+    }
+
+    toggleAll(state) {
+        state = typeof state === 'undefined' || state === true
+        return () => {
+            const cachedOptions = this.state.cachedOptions.map((option) => {
+                option.selected = state;
+                return option;
+            });
+            this.setState({ cachedOptions, allEnabled: state, allDisabled: !state })
+        }
     }
 
     renderElement(option) {
@@ -56,16 +78,29 @@ export default class HierarchySelector extends Component {
 
     render() {
         const option = this.props.option;
+        const {allEnabled, allDisabled} = this.getEnabledStatus();
 
         return (
-            <fieldset>
-                <legend>Select {option.name}</legend>
-                <ul className="hierarchy">
-                    {option.options.map(option => {
-                        return this.renderElement(option)
-                    })}
-                </ul>
-            </fieldset>
+            <form className="form">
+                <h1 className="margin-top--half margin-bottom">Select {option.name}</h1>
+                <div className="margin-bottom--2">
+                    <ToggleLink label="Enable all" enabled={!allEnabled} onClick={this.toggleAll(true)} />
+                    <ToggleLink label="Disable all" enabled={!allDisabled} onClick={this.toggleAll(false)} />
+                </div>
+
+                <div className="margin-top--4 margin-bottom--8">
+
+                    <ul className="hierarchy">
+                        {option.options.map(option => {
+                            return this.renderElement(option)
+                        })}
+                    </ul>
+
+                    <a className="btn btn--primary btn--thick btn--wide btn--big margin-right--half"
+                       onClick={this.saveSelections}>Save selection</a>
+                    <a className="btn btn--secondary btn--thick btn--wide btn--big" onClick={hashHistory.goBack}>Cancel</a>
+                </div>
+            </form>
         )
     }
 }
