803dd2f22ae414d10197b3b357e22eb2dcad89de
Crispioso <crispin.merriman@gmail.com>
2017-02-20 12:56:41 +0000

    Basic google analytics added

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { browserHistory } from 'react-router';
-import ga from 'ga-react-router';
 import { initStore } from './config/store';
+import analytics from './config/analytics';
 
 import App from "./components/App";
 import CSS from "./scss/main.scss";
 
 function init() {
 
-    const unlisten = browserHistory.listen(location => {
-        ga('send', location.pathname + location.search);
-    });
+    analytics.initialise();
 
     initStore(browserHistory, {
         // for alpha we will persist state in local storage
