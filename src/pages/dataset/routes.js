cee521136713dd59b8607933a323b13520670e9c
Marek <mkarwowski@equalexperts.com>
2017-02-21 15:33:26 +0000

    Routes updated to mimic V3 API endpoints

--- src/pages/dataset/routes.js
+++ src/pages/dataset/routes.js
@@ -9,7 +9,7 @@ import Dimensions from '../dimensions/components/Dimensions';
 export default [
     { path: `${config.BASE_PATH}/datasets/:id`, component: Details },
     { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version`, component: Version },
-    { path: `${config.BASE_PATH}/datasets/:id/download`, component: Download },
-    { path: `${config.BASE_PATH}/datasets/:id/dimensions`, component: Dimensions },
-    { path: `${config.BASE_PATH}/datasets/:id/dimensions/:dimensionID`, component: Dimension }
+    { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version/download`, component: Download },
+    { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version/dimensions`, component: Dimensions },
+    { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version/dimensions/:dimensionID`, component: Dimension }
 ]
