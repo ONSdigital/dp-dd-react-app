bd28a84ca6b77a037708aecbbaf91b134336dcbc
Marek <mkarwowski@equalexperts.com>
2017-03-06 13:35:05 +0000

    App routes configuration moved to a central place

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,18 @@
 import Home from '../pages/Home';
-import dataset from '../pages/dataset';
+
+import config from './';
+import Details from '../pages/dataset/components/DatasetDetails';
+import Version from '../pages/dataset/components/DatasetVersion';
+import Download from '../pages/dataset/components/Download';
+
+import Dimension from '../pages/dimension/components/Dimension';
+import Dimensions from '../pages/dimensions/components/Dimensions';
 
 export default [
     { path: '/', component: Home },
-    ...dataset.routes
+    { path: `${config.BASE_PATH}/datasets/:id`, component: Details },
+    { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version`, component: Version },
+    { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version/download`, component: Download },
+    { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version/dimensions`, component: Dimensions },
+    { path: `${config.BASE_PATH}/datasets/:id/editions/:edition/versions/:version/dimensions/:dimensionID`, component: Dimension }
 ]
